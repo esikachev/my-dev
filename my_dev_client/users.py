9bc01f98cbada27413d7fdf93a42750605d570b5
Alina Nesterova <anesterova@mirantis.com>
2016-12-15 17:29:22 +0400

    Update get request for user
    Fix getting user information

--- my_dev_client/users.py
+++ my_dev_client/users.py
@@ -1,5 +1,8 @@
+from flask import Flask
 import json
 import requests
+
+app = Flask(__name__)
 
 
 class Users(object):
@@ -9,11 +12,19 @@ class Users(object):
         url = 'http://localhost:5000/users'
         for (key, value) in kwargs.items():
             data += (key, value)
-        create_user = requests.post(url, data=json.dump(data))
-        return create_user.json()
+        create_user_request = requests.post(url, data=json.dump(data))
+        request_status = (create_user_request.status_code == requests.codes.ok)
+        if request_status is False:
+            raise Exception("Bad request in creating the user")
+        else:
+            return create_user_request.json()
 
     def get(self, user_id):
         """Get user information"""
         url = 'http://localhost:5000/users/%s' % user_id
-        get_user = requests.get(url)
-        return get_user.json()
+        get_user_request = requests.get(url)
+        request_status = (get_user_request.status_code == requests.codes.ok)
+        if request_status is False:
+            raise Exception("Bad request in getting the user")
+        else:
+            return get_user_request
