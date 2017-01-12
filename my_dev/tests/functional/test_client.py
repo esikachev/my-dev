85dc70eb2503ab72601e02098ee523f5d482ef9c
Alina Nesterova <anesterova@mirantis.com>
2017-01-12 17:59:35 +0400

    Added functional test to test creating of already existed user
    We should not create user with the username from DB, so it should
    not be created and have an error about this

--- my_dev/tests/functional/test_client.py
+++ my_dev/tests/functional/test_client.py
@@ -1,7 +1,9 @@
+from requests import exceptions as exc
 import sys
 
 import testtools
 
+from my_dev.tests.functional import utils
 import my_dev.users as users
 from my_dev import runner
 
@@ -13,11 +15,31 @@ class TestClient(testtools.TestCase):
         self.user = users.Users()
 
     def test_client(self):
+        user_name = utils.rand_name('test')
+        user = self.user.create(user_name, 'user@user.com', 'password')
+        self.user.get(user['id'])
         user = self.user.create('test-user', 'user@user.com', 'password')
         user_by_id = self.user.get(user['id'])
         user_by_name = self.user.get(user['username'])
         self.assertEqual(user_by_id, user_by_name)
         self.user.delete(user['id'])
+
+    def test_create_already_existed_user(self):
+        msg_exist = None
+        user_name = utils.rand_name('test')
+        error_message = "User exist with username: %s" % user_name
+        self.user.create(user_name, 'user@user.com', 'password')
+        try:
+            self.user.create(user_name, 'new@user.com', 'qwerty')
+        except exc.RequestException as e:
+            output_msg = str(e).splitlines()
+            for msg in output_msg:
+                if msg.lower() == error_message.lower():
+                    self.assertEqual(error_message.lower(), msg.lower())
+                    msg_exist = True
+            if not msg_exist:
+                raise BaseException('User with already existing username %s '
+                                    'have been created' % user_name)
 
     def test_init(self):
         sys.argv = ['my_dev/runner.py',
