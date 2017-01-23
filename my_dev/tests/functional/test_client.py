f8b463f0d9607337e352dbcd8f2844aad824c2bc
Evgeny Sikachev <esikachev@mirantis.com>
2017-01-23 14:09:48 +0400

    Fixed tests

--- my_dev/tests/functional/test_client.py
+++ my_dev/tests/functional/test_client.py
@@ -11,7 +11,7 @@ class TestClient(testtools.TestCase):
 
     def test_client(self):
         user = self.user.create('test-user', 'user@user.com', 'password')
-        id = self.user.get(user['id'])
-        name = self.user.get(user['username'])
-        self.assertEqual(id, name)
+        user_by_id = self.user.get(user['id'])
+        user_by_name = self.user.get(user['username'])
+        self.assertEqual(user_by_id, user_by_name)
         self.user.delete(user['id'])
