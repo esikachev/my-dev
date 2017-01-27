3c34518dea3c32a76efa8202c90826adfa61edcd
Alina Nesterova <anesterova@mirantis.com>
2017-01-27 12:56:11 +0400

    Move checks of existing user to users file

--- my_dev/tests/functional/test_client.py
+++ my_dev/tests/functional/test_client.py
@@ -1,7 +1,8 @@
 import sys
-
+from requests import exceptions as exc
 import testtools
 
+from my_dev.tests.functional import utils
 import my_dev.users as users
 from my_dev import runner
 
@@ -13,10 +14,11 @@ class TestClient(testtools.TestCase):
         self.user = users.Users()
 
     def test_client(self):
-        user = self.user.create('test-user', 'user@user.com', 'password')
-        user_by_id = self.user.get(user['id'])
-        user_by_name = self.user.get(user['username'])
-        self.assertEqual(user_by_id, user_by_name)
+        user_name = utils.rand_name('test')
+        user_email = utils.rand_name('test@')
+        user_password = utils.rand_name('pass')
+        user = self.user.create(user_name, user_email, user_password)
+        self.user.get(user['id'])
         self.user.delete(user['id'])
 
     def test_init(self):
@@ -24,3 +26,23 @@ class TestClient(testtools.TestCase):
                     'my', '--init', '-u', 'user', '-p', 'password',
                     '-e', 'email']
         runner.main()
+
+    def test_create_already_existed_user(self):
+        msg_exist = None
+        user_name = utils.rand_name('test')
+        user_email = utils.rand_name('user@')
+        user_password = utils.rand_name('pass')
+        error_message = "User exist with username: %s" % user_name
+        self.user.create(user_name, user_email, user_password)
+        try:
+            new_user_email = utils.rand_name('new_user@')
+            new_user_password = utils.rand_name('new_pass')
+            self.user.create(user_name, new_user_email, new_user_password)
+        except exc.RequestException as e:
+            output_msg = str(e)
+            for msg in output_msg:
+                if msg.lower() == error_message.lower():
+                    self.assertEqual(error_message.lower(), msg.lower())
+                    msg_exist = True
+            if not msg_exist:
+                raise BaseException('User exist with username: %s' % user_name)
--- my_dev/users.py
+++ my_dev/users.py
@@ -1,4 +1,7 @@
 import client
+from requests import exceptions as exc
+
+from my_dev import config
 
 
 class Users(object):
@@ -14,14 +17,23 @@ class Users(object):
             "email": email
         }
         request_create = self.client.post(self.prefix, data=data)
+        if (request_create.text == config.USER_EXISTS.format(
+                'username', data['username'])
+            or request_create.text == config.USER_EXISTS.format(
+                'email', data['email'])):
+            raise exc.RequestException(request_create.text)
         return request_create
 
     def get(self, user_id):
         request_get = self.client.get(self.prefix, user_id)
+        if request_get.text == config.USER_DOESNT_EXIST.format(id):
+            raise exc.RequestException(request_get.text)
         return request_get
 
     def delete(self, user_id):
         request_delete = self.client.delete(self.prefix, user_id)
+        if request_delete.text == config.USER_DOESNT_EXIST.format(id):
+            raise exc.RequestException(request_delete.text)
         return request_delete
 
     def get_id(self, name):
