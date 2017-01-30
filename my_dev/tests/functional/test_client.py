0e1094c4cb074edb4eff7f55f399a885c218dd1e
Alina Nesterova <anesterova@mirantis.com>
2017-01-30 20:21:23 +0400

    Fixed tests

--- my_dev/tests/functional/test_client.py
+++ my_dev/tests/functional/test_client.py
@@ -1,4 +1,3 @@
-from requests import exceptions as exc
 import sys
 
 import testtools
@@ -19,24 +18,22 @@ class TestClient(testtools.TestCase):
         user_email = utils.rand_name('test')
         user_pass = utils.rand_name('test')
         user = self.user.create(user_name, user_email, user_pass)
-        self.user.get(user['id'])
-        user = self.user.create('test-user', 'user@user.com', 'password')
         user_by_id = self.user.get(user['id'])
         user_by_name = self.user.get(user['username'])
         self.assertEqual(user_by_id, user_by_name)
         self.user.delete(user['id'])
 
     def test_create_already_existed_user(self):
-        msg_exist = None
         user_name = utils.rand_name('test')
         user_email = utils.rand_name('test')
         user_pass = utils.rand_name('test')
-        error_message = "User exist with username: %s" % user_name
         self.user.create(user_name, user_email, user_pass)
         new_user_email = utils.rand_name('test')
         new_user_pass = utils.rand_name('test')
         response = self.user.create(user_name, new_user_email, new_user_pass)
-        print response.status_code
+        self.assertEqual(400, response.get('status_code'))
+        self.assertEqual('User exist with: %s' % user_name,
+                         response.get('message'))
 
     def test_init(self):
         sys.argv = ['my_dev/runner.py',
