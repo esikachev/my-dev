ac57ac0d426fb7382ab947dd6cae9797a3b6a77f
Alina Nesterova <anesterova@mirantis.com>
2017-01-31 11:00:16 +0400

    Added checking of duplicate user(#9)
    We need to tests reaction of server on negative scenarios

--- my_dev/tests/functional/test_client.py
+++ my_dev/tests/functional/test_client.py
@@ -2,6 +2,7 @@ import sys
 
 import testtools
 
+from my_dev.tests.functional import utils
 import my_dev.users as users
 from my_dev import runner
 
@@ -13,11 +14,26 @@ class TestClient(testtools.TestCase):
         self.user = users.Users()
 
     def test_client(self):
-        user = self.user.create('test-user', 'user@user.com', 'password')
+        user_name = utils.rand_name('test')
+        user_email = utils.rand_name('test')
+        user_pass = utils.rand_name('test')
+        user = self.user.create(user_name, user_email, user_pass)
         user_by_id = self.user.get(user['id'])
         user_by_name = self.user.get(user['username'])
         self.assertEqual(user_by_id, user_by_name)
         self.user.delete(user['id'])
+
+    def test_create_already_existed_user(self):
+        user_name = utils.rand_name('test')
+        user_email = utils.rand_name('test')
+        user_pass = utils.rand_name('test')
+        self.user.create(user_name, user_email, user_pass)
+        new_user_email = utils.rand_name('test')
+        new_user_pass = utils.rand_name('test')
+        response = self.user.create(user_name, new_user_email, new_user_pass)
+        self.assertEqual(400, response.get('status_code'))
+        self.assertEqual('User exist with: %s' % user_name,
+                         response.get('message'))
 
     def test_init(self):
         sys.argv = ['my_dev/runner.py',
