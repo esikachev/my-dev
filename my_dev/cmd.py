2ff60752d6e39414a4ec4e5419686ab45966c213
Evgeny Sikachev <esikachev@mirantis.com>
2017-02-09 00:41:39 +0400

    [WIP] We need to correct handle of ssh to only host or alias

--- my_dev/cmd.py
+++ my_dev/cmd.py
@@ -3,13 +3,9 @@ import os
 
 class Cmd(object):
     def ssh_cmd(self, ssh_username, ssh_password, host):
-        separator = ''
-        if ssh_username:
-            separator = '@'
         base_cmd = ('sshpass -p {ssh_password} ssh '
-                    '{ssh_username}{separator}'
+                    '{ssh_username}@'
                     '{host}'.format(ssh_username=ssh_username,
-                                    separator=separator,
                                     ssh_password=ssh_password,
                                     host=host))
         self.run(base_cmd)
--- my_dev/tests/cli/test_cli.py
+++ my_dev/tests/cli/test_cli.py
@@ -10,30 +10,45 @@ from my_dev.tests import utils
 class TestCli(testtools.TestCase):
     def setUp(self):
         super(TestCli, self).setUp()
-        self.username = utils.rand_name('user')
-        self.email = utils.rand_name('email')
+        username = utils.rand_name('user')
+        email = utils.rand_name('email')
+        self.cmd = 'sshpass -p test_pass ssh user@127.0.0.1'
+        self.init_user(username, email)
 
-    def init_user(self):
+    def init_user(self, username, email):
         sys.argv = ['my_dev/runner.py',
-                    'my', '--init', '-u', self.username, '-p', 'password',
-                    '-e', self.email]
+                    'my', '--init', '-u', username, '-p', 'password',
+                    '-e', email]
         runner.main()
 
     @mock.patch('os.system')
     @mock.patch('__builtin__.raw_input', return_value='alias')
     @mock.patch('my_dev.base.Base.enter_pass', return_value='test_pass')
     def test_workflow(self, mock_pass, mock_alias, mock_os):
-        self.init_user()
-
         sys.argv = ['my', 'ssh', 'user@127.0.0.1']
         runner.main()
-        mock_os.assert_called_with('sshpass -p test_pass ssh user@127.0.0.1')
+        mock_os.assert_called_with(self.cmd)
+
+    @mock.patch('os.system')
+    @mock.patch('__builtin__.raw_input', return_value='alias')
+    @mock.patch('my_dev.base.Base.enter_pass', return_value='test_pass')
+    def test_workflow(self, mock_pass, mock_alias, mock_os):
         sys.argv = ['my', 'ssh', '127.0.0.1']
         runner.main()
-        mock_os.assert_called_with('sshpass -p test_pass ssh user@127.0.0.1')
+        mock_os.assert_called_with(self.cmd)
+
+    @mock.patch('os.system')
+    @mock.patch('__builtin__.raw_input', return_value='alias')
+    @mock.patch('my_dev.base.Base.enter_pass', return_value='test_pass')
+    def test_workflow(self, mock_pass, mock_alias, mock_os):
         sys.argv = ['my', 'ssh', 'user@127.0.0.1']
         runner.main()
-        mock_os.assert_called_with('sshpass -p test_pass ssh user@127.0.0.1')
+        mock_os.assert_called_with(self.cmd)
+
+    @mock.patch('os.system')
+    @mock.patch('__builtin__.raw_input', return_value='alias')
+    @mock.patch('my_dev.base.Base.enter_pass', return_value='test_pass')
+    def test_workflow(self, mock_pass, mock_alias, mock_os):
         sys.argv = ['my', 'ssh', 'alias']
         runner.main()
-        mock_os.assert_called_with('sshpass -p test_pass ssh user@127.0.0.1')
+        mock_os.assert_called_with(self.cmd)
