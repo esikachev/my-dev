9b209d0702211ad71357f6625fc58655f7258941
Evgeny Sikachev <esikachev@mirantis.com>
2017-02-18 15:21:52 +0300

    added missed arg

--- my_dev/tests/unit/test_ssh.py
+++ my_dev/tests/unit/test_ssh.py
@@ -7,7 +7,7 @@ from my_dev import ssh
 
 class TestSsh(testtools.TestCase):
     @mock.patch('my_dev.client.Client', return_value='client')
-    def test_init(self):
+    def test_init(self, mock_client):
         ssh_client = ssh.Ssh('user_id')
         self.assertEqual('client', ssh_client.client)
         self.assertEqual('user_id', ssh_client.user_id)
